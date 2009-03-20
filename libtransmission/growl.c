adf70dd81cdeb1424d4814daa4b6bd5288d59a17
liesen <liesen@debian.(none)>
2009-03-20 01:37:26 +0100

    added skeleton implementation for growl callback

---
+++ libtransmission/growl.c
@@ -0,0 +1,10 @@
+#include <libgrowl.h>
+
+#include "transmission.h"
+#include "growl.h"
+
+void
+tr_torrentSetGrowlCompletionCallback(tr_torrent * tor,
+                                     tr_completeness completeness,
+                                     void * user_data) {
+}
---
+++ libtransmission/growl.h
@@ -0,0 +1,6 @@
+#include "transmission.h"
+
+void
+tr_torrentSetGrowlCompletionCallback(tr_torrent * tor,
+                                     tr_completeness completeness,
+                                     void * user_data);
